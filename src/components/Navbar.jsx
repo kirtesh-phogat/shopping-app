751e0d01b4daadc669943ee655b1a28258c1ecd4
kirteshphogat <phogatk55@gmail.com>
2026-05-13 22:36:48 +0530

    mobile view fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Link, NavLink } from "react-router";
+import { Link, NavLink } from "react-router-dom";
 // eslint-disable-next-line no-unused-vars
 import { AnimatePresence, motion } from "motion/react";
 import useAuth from "../hook/useAuth";
@@ -7,10 +7,10 @@ import { useSelector } from "react-redux";
 
 const Navbar = () => {
   const { isLoggedIn } = useAuth();
+  const [isNavOpen, setNavOpen] = useState(false);
 
   // const products = useSelector((state) => state.cart.cartItem);
-  const products = useSelector(
-  (state) => state.cart.cartItem || [] );
+  const products = useSelector((state) => state.cart.cartItem || []);
   return (
     <>
       <nav className="bg-white sticky top-0 z-10 w-full border-gray-200 dark:bg-gray-900">
@@ -38,6 +38,7 @@ const Navbar = () => {
               </>
             )}
             <button
+              onClick={() => setNavOpen(!isNavOpen)}
               data-collapse-toggle="navbar-user"
               type="button"
               className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
@@ -63,7 +64,9 @@ const Navbar = () => {
             </button>
           </div>
           <div
-            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
+              isNavOpen ? "block" : "hidden"
+            }`}
             id="navbar-user"
           >
             <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
